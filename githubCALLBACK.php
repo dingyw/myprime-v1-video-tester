014d400fa904bca51905515b3258475b510187a3
qngo-windwalker <quan.ngo@windwalker.com>
2013-03-14 19:21:21 -0400

    Added more text to git hook.

--- githubCALLBACK.php
+++ githubCALLBACK.php
@@ -4,7 +4,7 @@ echo "<pre>Starting up...\n";
 //ignore_user_abort();
 $i =  exec('git pull origin master'); 
 echo $i;
-error_log("Incoming Github" . $i,1,"quan.ngo@windwalker.com","From: quan.ngo@windwalker.com\nSubject: myPrime video Utility Incoming GitHub"); 
+error_log("myPrime Video Utility: Incoming Github" . $i,1,"quan.ngo@windwalker.com","From: quan.ngo@windwalker.com\nSubject: myPrime video Utility Incoming GitHub"); 
 echo "</pre>";
 
 ?>
